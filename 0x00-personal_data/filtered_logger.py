21879eb1300d4aa997a75984978302fa5c451e22
Hezekiah Oyemade <oyedey@gmail.com>
2023-08-04 05:23:07 +0100

    task 0: logging

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""filter_datum that returns the log message obfuscated"""
+
+
+import os
+from typing import List
+import logging
+import re
+import mysql.connector
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
+    """returns the log message filtered"""
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format a log record"""
+        msg_log = super(RedactingFormatter, self).format(record)
+        txt_str = filter_datum(self.fields, self.REDACTION, msg_log, self.SEPARATOR)
+        return txt_str

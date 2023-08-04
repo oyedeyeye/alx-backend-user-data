0e47a94fca5f596238254a6a643846cc5a642be5
Hezekiah Oyemade <oyedey@gmail.com>
2023-08-04 13:36:51 +0100

    task 5: Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+hash_password function that expects one string argument name password
+and returns a salted, hashed password, which is a byte string.
+"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """returns a salted, hashed password, which is a byte string"""
+    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())

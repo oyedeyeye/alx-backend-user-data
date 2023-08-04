40f0aee10a3e4da32b129bab31f851f4fe1b39dd
Hezekiah Oyemade <oyedey@gmail.com>
2023-08-04 14:34:35 +0100

    task 3. Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import os
 from typing import List, Dict, Tuple
 import logging
 import re
-from mysql.connector import (connection)
+from mysql.connector
 
 
 PII_FIELDS: Tuple[str] = ("name", "email", "phone", "ssn", "password")
@@ -39,7 +39,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_name: str = os.getenv("PERSONAL_DATA_DB_NAME", "")
     db_user: str = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd: str = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    connector = connection.MySQLConnection(
+    connector = mysql.connector.connection.MySQLConnection(
         host=db_host,
         port=3306,
         user=db_user,
